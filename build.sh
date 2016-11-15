f4a27cf56e1fcac9990a4440fe18bbd378b86c93
farhan687 <farhan687@gmail.com>
2016-11-15 09:50:56 +0530

    Fix travis

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ fi
 # enable error reporting to the console
 set -e 
 
-python shellcommit.py
+python generate.py
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone https://${GH_TOKEN}@github.com/farhan687/appbaseio.github.io.git ../farhan687.github.io.master
 
 # copy generated HTML site to `master' branch
-cp -R index.html ../farhan687.github.io.master
+cp -R ./index.html ./assets/* repo_images/* ../farhan687.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
@@ -23,4 +23,4 @@ git config user.email "farhan687@gmail.com"
 git config user.name "Farhan Chauhan"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin master > /dev/null 2>&1
+git push origin master
