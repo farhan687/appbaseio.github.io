dbae4bb2d5e1c331fc46a29aa5f18ff210bffb98
farhan687 <farhan687@gmail.com>
2016-11-15 09:53:14 +0530

    Fix travis

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ fi
 # enable error reporting to the console
 set -e 
 
-python generate.py
+# python generate.py
 
 #clone `master' branch of the repository using encrypted GH_TOKEN for authentification
 git clone https://${GH_TOKEN}@github.com/farhan687/appbaseio.github.io.git ../farhan687.github.io.master
